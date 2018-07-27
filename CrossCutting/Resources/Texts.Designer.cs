a6c11d6558b65ab82fc8d59f595ef21ac5a42a73
Rafael Ferreira Garcia <rafaelfgx@gmail.com>
2018-07-26 21:31:52 -0300

    Code improvements.

--- CrossCutting/Resources/Texts.Designer.cs
+++ CrossCutting/Resources/Texts.Designer.cs
@@ -8,10 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Solution.CrossCutting.Resources {
+namespace Solution.CrossCutting.Resources
+{
     using System;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,85 +23,103 @@ namespace Solution.CrossCutting.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Texts {
-        
+    public class Texts
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Texts() {
+        internal Texts()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
+        public static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Solution.CrossCutting.Resources.Texts", typeof(Texts).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
-            get {
+        public static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Login or password invalid!.
         /// </summary>
-        public static string AuthenticationInvalid {
-            get {
+        public static string AuthenticationInvalid
+        {
+            get
+            {
                 return ResourceManager.GetString("AuthenticationInvalid", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to An unexpected error occurred, try the operation again or contact your system administrator..
         /// </summary>
-        public static string Error {
-            get {
+        public static string Error
+        {
+            get
+            {
                 return ResourceManager.GetString("Error", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Choose a file..
         /// </summary>
-        public static string FileChoose {
-            get {
+        public static string FileChoose
+        {
+            get
+            {
                 return ResourceManager.GetString("FileChoose", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Invalid extension!.
         /// </summary>
-        public static string FileInvalidExtension {
-            get {
+        public static string FileInvalidExtension
+        {
+            get
+            {
                 return ResourceManager.GetString("FileInvalidExtension", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Invalid size!.
         /// </summary>
-        public static string FileInvalidSize {
-            get {
+        public static string FileInvalidSize
+        {
+            get
+            {
                 return ResourceManager.GetString("FileInvalidSize", resourceCulture);
             }
         }
